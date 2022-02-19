cd9b5bb8819a42579fcb128956738cdcd9728dee
Artem Boyarskov <arboyarskov@ya.ru>
2022-02-19 16:24:55 +0300

    2. Каркас шахматной доски [#792]

---
+++ chess/src/test/java/ru/job4j/chess/FigureTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.chess;
+
+import org.junit.Test;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.black.BishopBlack;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class FigureTest {
+
+    @Test
+    public void whenPositionCorrect() {
+       BishopBlack bishopBlack = new BishopBlack(Cell.F7);
+       assertThat(bishopBlack.position(), is(Cell.F7));
+    }
+
+    @Test
+    public void whenCopyCorrect() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.A3);
+        assertThat(bishopBlack.copy(Cell.A3).position(), is(Cell.A3));
+    }
+
+    @Test
+    public void whenWayCorrect() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        Cell[] cells = new Cell[] {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        assertThat(bishopBlack.way(Cell.G5), is(cells));
+    }
+}
